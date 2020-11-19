eef46e42c0e2708d3030081933a7e7c4696e96b1
Veera Sauerland <veera.sauerland@tuni.fi>
2020-11-19 12:46:05 +0200

    Halloween map added

---
+++ Halloween map/Assets/scripts/CoinPicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinPicker : MonoBehaviour
+{
+    public GameObject particleFX;
+
+    private float Coin = 0;
+
+    public TextMeshProUGUI textCoins;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       
+     if(other.transform.tag == "Coin")
+        {
+            Coin ++;
+            textCoins.text = Coin.ToString();
+            Instantiate(particleFX, transform.position, transform.rotation);
+
+            Destroy(other.gameObject);
+        }   
+    }
+
+}
